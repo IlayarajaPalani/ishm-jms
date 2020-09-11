dd19f66f162fbb2cf65f099cc13cd7c51cc3b0ec
Ilaya <ilayarajapalani@gmail.com>
2020-09-10 20:02:19 -0400

    Adding the spring jms project

---
+++ src/test/java/com/springframework/ishmjms/IshmJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.ishmjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IshmJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
